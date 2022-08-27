2bc36cec160dcf5e06665da59440bf341833e44a
Вячеслав Андрющенко <31081994@mail.ru>
2022-08-27 16:16:40 +0700

    ready_2

--- itog.py
+++ itog.py
@@ -57,45 +57,46 @@ vk_client = VkUser(token, '5.131')
 photo_vk = vk_client.photos_get('1')
 
 
-# TOKEN = "AQAEA7qj0SyLAADLWxJK0PtrYUrqkqZl0aVPcxY"
+TOKEN = "AQAEA7qj0SyLAADLWxJK0PtrYUrqkqZl0aVPcxY"
 
 
-# class YandexDisk:
+class YandexDisk:
 
-#     def __init__(self, token):
-#         self.token = token
+    def __init__(self, token):
+        self.token = token
 
-#     def get_headers(self):
-#         return {
-#             'Content-Type': 'application/json',
-#             'Authorization': f'OAuth {self.token}'
-#         }
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
 
    
     
-    def _get_upload_link(self, disk_file_path):
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+    # def _get_upload_link(self, disk_file_path):
+    #     upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+    #     headers = self.get_headers()
+    #     params = {"path": disk_file_path, "overwrite": "true"}
+    #     response = requests.get(upload_url, headers=headers, params=params)
+    #     pprint(response.json())
+    #     return response.json()
+
+
+    def upload_file_to_disk(self):        
+        # href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
         headers = self.get_headers()
-        params = {"path": disk_file_path, "overwrite": "true"}
-        response = requests.get(upload_url, headers=headers, params=params)
-        pprint(response.json())
-        return response.json()
-
-
-    def upload_file_to_disk(self, disk_file_path):        
-        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
-
-        response = requests.put('https://sun4-17.userapi.com/impf/byTiuiCzlcw-7HQqNI8VeOhaKVPrGm-Sdt9wsQ/banGDBotKVM.jpg?size=731x1000&quality=96&sign=432b2466a268e6e51f46eb47c80ce26a&c_uniq_tag=IraiqwN4-ugqtJPjK4lTRIFF5_YM2KEiJGJmVi9HjtY&type=album', '56')
+        upload_url = 'https://sun4-17.userapi.com/impf/byTiuiCzlcw-7HQqNI8VeOhaKVPrGm-Sdt9wsQ/banGDBotKVM.jpg?size=731x1000&quality=96&sign=432b2466a268e6e51f46eb47c80ce26a&c_uniq_tag=IraiqwN4-ugqtJPjK4lTRIFF5_YM2KEiJGJmVi9HjtY&type=album'
+        response = requests.post(headers=headers, url=upload_url)
         response.raise_for_status()
         if response.status_code == 201:
             print('Загружено')
 
-ya = yadisk.YaDisk(token=TOKEN)
-# ya = YandexDisk(token=TOKEN)
-pprint(photo_vk)
-for key, send in photo_vk.items():
-    ya.upload_url(send, key)
-# pprint(ya.get_files_list())
+# ya = yadisk.YaDisk(token=TOKEN)
+ya = YandexDisk(token=TOKEN)
+# pprint(photo_vk)
+# for key, send in photo_vk.items():
+#     ya.upload_url(send, key)
+pprint(ya.upload_file_to_disk())
 # pprint(ya.upload_file_to_disk('56'))
 
 
--- itog_1.py
+++ itog_1.py
@@ -2,24 +2,22 @@ import requests
 from pprint import pprint
 import json
 import yadisk
-import sys
+from datetime import datetime as dt
+from tqdm import tqdm
 
 
-
-
-with open("token.json", "r") as read_file:
+with open("token.json", "r", encoding='utf-8') as read_file:
     token = json.load(read_file)
 
 
 class VkUser:
     url = 'https://api.vk.com/method/'
+
     def __init__(self, token, version):
         self.params = {
             'access_token': token,
-            'v': version    
+            'v': version
         }
-
-      
 
     def photos_get(self, owner_id=input('Введите id пользователя '), count=5):
         get_photo = self.url + 'photos.get'
@@ -32,48 +30,55 @@ class VkUser:
             'type': 'z',
             'rev': '1'
         }
-        
-        list_photo = []
-        dict_photo = {}
-        photos = [{}]
-        req = requests.get(get_photo, params={**self.params, **get_photo_params}).json()
-        req = req['response']['items']
-        for z in req:
-            req = z['sizes']
-            for size in req:                
-                if size['type'] == 'z':
-                    list_photo.append(size['url'])
-                    dict_photo[z['likes']['count']] = size['url']
-                    photos[0]["file_name:"] = f"{z['likes']['count']}.jpg"
-                    photos[0]["size"] = size['type']
-                    with open('data.json', 'a') as f:
-                        json.dump(f'{photos}', f)  
 
-        
-            # print(photos)
-        
-        return dict_photo
+        photos = {}
+        data = []
+        names = []
+        req = requests.get(get_photo, params={**self.params, **get_photo_params}).json()
+        items = req['response']['items']
+        counter = 0
+        for item in items:
+            sizes = item['sizes']            
+            for size in sizes:
+                if size['type'] == 'z':
+                    photos[counter] = {}
+                    photos[counter]['name'] = str(item['likes']['count'])
+                    names.append(photos[counter]['name'])
+                    photos[counter]['url'] = size['url']
+                    photos[counter]['date'] = dt.utcfromtimestamp(item['date']).strftime('%Y-%m-%d')
+                    photos[counter]['size'] = 'z'
+                    counter += 1
+
+        for name in set(names):
+            if names.count(name) >= 2:
+                for photo in photos.values():
+                    if photo['name'] == name:
+                        photo['name'] = f"{photo['name']} {photo['date']}"
+
+        for photo in photos.values():
+            data.append({"file_name": f"{photo['name']}.jpg",
+                        "size": photo['size']})
+
+        with open('data.json', 'w', encoding='utf-8') as file:
+            json.dump(f'{data}', file)
+
+        return photos
 
 
 vk_client = VkUser(token, '5.131')
 photo_vk = vk_client.photos_get()
 
-
-
-
-# TOKEN = ""
-
-
 ya = yadisk.YaDisk(token=input('Вставьте токен YaDisc '))
-name_folder = input("Введите название папки которую хотите создать ")
-folder = ya.mkdir(name_folder)
+dirs = []
+for i in ya.listdir('/'):
+    if i['file'] is None:
+        dirs.append(i['name'])
 
+name_folder = input("Введите название папки, в которую хотите добавить фото: ")
+if name_folder not in dirs:
+    ya.mkdir(name_folder)    
 
-x = 0 
-for key, send in photo_vk.items():
-    ya.upload_url(f'{send}',f'{name_folder}/{key}')
-    print(f'Загружено {x} фото из {len(photo_vk)-1}')
-    x +=1
+for photo in tqdm(photo_vk.values()):
+    ya.upload_url(f"{photo['url']}", f"{name_folder}/{photo['name']}")
 
 print('Фото успешно загружены')
-
